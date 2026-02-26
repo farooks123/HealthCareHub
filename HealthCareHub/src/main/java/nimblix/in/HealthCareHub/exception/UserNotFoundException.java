8d9d51a380cc0c320b10db213c87f9a1f025c15d
farooks123 <umarfarookvj@gmail.com>
2026-02-26 11:18:20 +0530

    Implemented UserNotFoundException fix and updated APIs

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/exception/UserNotFoundException.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/exception/UserNotFoundException.java
@@ -1,7 +1,8 @@
 package nimblix.in.HealthCareHub.exception;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String s){
-        super("User not found");
+    public UserNotFoundException(String message) {
+        super(message);
     }
+
 }
